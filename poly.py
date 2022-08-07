77d63702232fe8248457bb16b194f8cadc5d5c57
Valeriy Schepak <vschepak@gmail.com>
2022-08-07 17:03:51 +0300

    Пример полиморфизма

---
+++ poly.py
@@ -0,0 +1,35 @@
+class Man:
+    def __init__(self, name):
+        self.name = name
+    def greet(self):
+        print(f"Привет! Я человек {self.name}!")
+
+class Worker(Man):
+    def greet(self):
+        print(f"Привет! Я работник {self.name}!")
+
+class Programmer(Worker):
+    def greet(self):
+        print(f"Привет! Я программист {self.name}!")
+
+a = Man("Valera")
+b = Worker("Ivan")
+c = Programmer("Petr")
+
+a.greet()
+b.greet()
+c.greet()
+
+
+
+
+
+
+
+
+
+
+
+
+
+#b._B__private()
